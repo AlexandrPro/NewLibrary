493e487beb2d05148ce0ef982746a15dc6ff3116
Prokofiev Alexander <Alexandr.Prokofiev.A@gmail.com>
2017-09-08 15:21:12 +0300

    remove all js code to js files

---
+++ Library.MVC/Scripts/PublishingHouse/Edit.js
@@ -0,0 +1,37 @@
+﻿$(document).ready(function () {
+
+    //alert(JSON.stringify(selectedValues));
+
+    $("#multiSelect").kendoMultiSelect({
+        placeholder: "Select products...",
+        dataTextField: "Name",
+        dataValueField: "Id",
+        dataSource: {
+            transport: {
+                read: {
+                    dataType: "json",
+                    url: "/PublishingHouse/Books_Read",
+                }
+            }
+        },
+        value: selectedValues,
+    });
+});
+
+var selectedValues = $.ajax({
+    type: "POST",
+    dataType: "json",
+    url: "/PublishingHouse/SelectedBooks_Read",
+    data: { id: GetCurentPuclishingHouseId() },
+});
+selectedValues.done(function (data) {
+    $("#multiSelect").data("kendoMultiSelect").value(data);
+});
+
+
+function GetCurentPuclishingHouseId() {
+    var url = window.location.href;
+    var curentId = url.substr(url.lastIndexOf('/') + 1, url.length);
+    //alert(curentId);
+    return curentId;
+}
